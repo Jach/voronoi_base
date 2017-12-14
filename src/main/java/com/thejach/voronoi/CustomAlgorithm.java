dac2f9c69b733695eaf48e66cbaf001a4e52cc27
Kevin Secretan <kevinmccloud@gmail.com>
2017-12-13 17:51:37 -0800

    Add formula comments / tips

--- src/main/java/com/thejach/voronoi/CustomAlgorithm.java
+++ src/main/java/com/thejach/voronoi/CustomAlgorithm.java
@@ -2,8 +2,8 @@ package com.thejach.voronoi;
 
 public class CustomAlgorithm implements VoronoiAlgorithm {
 
-  private Points points;
-  private BoundingBox boundingBox;
+  private final Points points;
+  private final BoundingBox boundingBox;
   private Graph graph;
 
   /**
@@ -18,21 +18,36 @@ public class CustomAlgorithm implements VoronoiAlgorithm {
   @Override
   public void generate() {
     // Problem statement:
+
     // Observe the behavior of Voronoi diagrams generated from two points.
     // The result is an orthogonal line equidistant between them that bisects the plane
     // defined by the bounding box that the points are in.
     // Given two points defined in points.xs, points.ys, change the code below
     // so that the graph structure contains one edge line: the line that bisects
     // the plane between the two points.
+
+    // Useful formulas:
+    // Midpoint formula between two points (x0, y0) and (x1, y1) is the point ( (x0+x1)/2, (y0+y1)/2 )
+    // The slope-form equation of a line is: y - b = m(x -a)
+    //                                       inputs are m, the line slope, and the point (a, b)
+    // The slope-intersect equation of a line is: y = mx + b
+    //                                       inputs are m, the line slope, and b, the y-intercept
+
+    // Tip: the Canvas gracefully handles out-of-bounds edge points and draws the right line anyway
+
     graph = new Graph();
-    
+
     // Replace this code with your own!
     graph.addEdge(new Edge(boundingBox.minX, boundingBox.minY,
-                           boundingBox.maxX, boundingBox.maxY));
+            boundingBox.maxX, boundingBox.maxY));
     graph.addEdge(new Edge(boundingBox.minX, boundingBox.maxY,
-                           boundingBox.maxX, boundingBox.minY));
-    graph.addEdge(new Edge(points.xs[0], points.ys[0],
-                           points.xs[1], points.ys[1]));
+            boundingBox.maxX, boundingBox.minY));
+    double x0 = points.xs[0];
+    double y0 = points.ys[0];
+    double x1 = points.xs[1];
+    double y1 = points.ys[1];
+    graph.addEdge(new Edge(x0, y0,
+            x1, y1));
   }
 
   @Override
