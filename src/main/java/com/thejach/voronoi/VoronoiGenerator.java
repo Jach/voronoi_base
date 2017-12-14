537b2e5f53e73befc03e2722b955f1ae3c029f7d
Kevin Secretan <kevinmccloud@gmail.com>
2017-12-13 17:58:07 -0800

    Add helpful debug prints

--- src/main/java/com/thejach/voronoi/VoronoiGenerator.java
+++ src/main/java/com/thejach/voronoi/VoronoiGenerator.java
@@ -10,12 +10,17 @@ public class VoronoiGenerator {
     } else if ("custom".equals(algorithm)) {
       algorithmImpl = new CustomAlgorithm(points, boundingBox);
     }
+    System.out.println("Using " + algorithm);
+    System.out.println(String.format("First two points: (%f,%f) and (%f,%f)", points.xs[0], points.ys[0], points.xs[1], points.ys[1]));
 
     algorithmImpl.generate();
   }
 
   public Graph getGraph() {
-    return algorithmImpl.getGraph();
+    Graph g = algorithmImpl.getGraph();
+    Edge e = g.getEdges().get(0);
+    System.out.println(String.format("First edge: (%f,%f) to (%f,%f)", e.getX0(), e.getY0(), e.getX1(), e.getY1()));
+    return g;
   }
 
 }
